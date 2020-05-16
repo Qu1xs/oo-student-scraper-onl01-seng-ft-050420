6dcb2d7ea01f9038efd438ba95f8830994f856ac
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-16 17:21:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,20 +6,21 @@ class Scraper
   attr_accessor :students
 
   def self.scrape_index_page(index_url)
-
-    index_html = open(index_url)
-    index_doc = Nokogiri::HTML(index_html)
-    student_cards = index_doc.css(".student-card")
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+    student_cards = doc.css(".student-card")
     students = []
-    student_cards.collect do |student_card_xml|
+    
+    student_cards.map do |card|
       students << {
-        :name => student_card_xml.css("h4.student-name").text,
-        :location => student_card_xml.css("p.student-location").text,
-        :profile_url => "./fixtures/student-site/" + student_card_xml.css("a").attribute("href").value
-        }
+        :name => card.css("h4.student-name"),
+        :location => card.css("p.student-location"),
+        :profile_url => "./fixtures/student-site/" + card.css("a").attribute("href").value
+      }
     end
     students
   end
+
   def self.scrape_profile_page(profile_url)
     
   end
